d73855a1dcb187ac62b4e01600d23e023ace1055
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-05-01 07:26:11 +0545

    Enhance ContactUs component with icons and improved layout for better user engagement

--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import emailjs from '@emailjs/browser';
+import { FaHome, FaMapSigns, FaPhone } from 'react-icons/fa';
 
 const ContactUs = () => {
     const [formData, setFormData] = useState({
@@ -45,7 +46,7 @@ const ContactUs = () => {
         }
     }
         return (
-            <section className='flexic flex-col pt-10'>
+            <section className='flexic flex-col pt-10 gap-y-30'>
                 <div className='flexic flex-col text-center space-y-2'>
                     <div className='space-y-2 mb-10'>
                         {submitStatus && (
@@ -102,34 +103,35 @@ const ContactUs = () => {
                         </div>
                     </form>
                 </div>
-                <div>
-                    <div>
-                        <div>
-                            Logo
+                <div className='w-full h-50 flexic justify-center bg-red-600 gap-x-20 '>
+                    <div className='flexic flex-col justify-center text-center '>
+                        <div className='m-5 bg-white p-10 -mt-30 rounded-full text-yellow-500 bg-gradient-to-r from-pink-300 to-rose-600'>
+                            <FaHome className='text-5xl' />
                         </div>
-                        <div>
-                            <h1>About Resturant</h1>
+                        <div className='text-yellow-500 space-x-3'>
+                            <h1 className='text-3xl'>Our Resturant</h1>
                             <p>Best Resturant in Pokhara</p>
                         </div>
                     </div>
-                    <div>
-                        <div>
-                            Logo
+                    <div className='flexic flex-col justify-center text-center '>
+                        <div className='m-5 bg-white p-10 -mt-30 rounded-full text-yellow-500 bg-gradient-to-r from-pink-300 to-rose-600'>
+                            <FaPhone className='text-5xl' />
                         </div>
-                        <div>
-                            <h1>About Resturant</h1>
-                            <p>Best Resturant in Pokhara</p>
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                            Logo
-                        </div>
-                        <div>
-                            <h1>About Resturant</h1>
-                            <p>Best Resturant in Pokhara</p>
+                        <div className='text-yellow-500 space-x-3'>
+                            <h1 className='text-3xl'>Phone</h1>
+                            <p>+977-9746311761</p>
                         </div>
                     </div>
+                    <div className='flexic flex-col justify-center text-center '>
+                        <div className='m-5 bg-white p-10 -mt-30 rounded-full text-yellow-500 bg-gradient-to-r from-pink-300 to-rose-600'>
+                            <FaMapSigns className='text-5xl' />
+                        </div>
+                        <div className='text-yellow-500 gap-x-10'>
+                            <h1 className='text-3xl'>Location</h1>
+                            <p>Panthi Galli, Pokhara</p>
+                        </div>
+                    </div>
+                    
                 </div>
             </section>
         )
